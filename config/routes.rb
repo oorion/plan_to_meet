2b1eca6c702ae03e8541e21fea8bc0dafe6424f7
Orion Osborn <orion.osborn@gmail.com>
2015-03-01 00:31:59 -0700

    add users show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  resources :users
+  resources :users do
+    get 'preferences', to: 'users/preferences#index'
+  end
 
   post 'login', to: 'sessions#create', as: 'login'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
