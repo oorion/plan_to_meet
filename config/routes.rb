bde0a16982f16bddf02762e9efb26cdb11c46818
Orion Osborn <orion.osborn@gmail.com>
2015-02-28 21:25:44 -0700

    More styling changes.  Also updated form to include url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "welcome/index"
-  root "welcome#index"
+  get 'welcome/index'
+  root 'welcome#index'
 
   resources :users
+
+  post 'login', to: 'sessions#create', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
