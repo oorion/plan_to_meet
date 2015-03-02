992f9a027ebb64008a889ded8fd5cd2449eba2e3
Orion Osborn <orion.osborn@gmail.com>
2015-03-02 16:54:04 -0700

    update database configuration to use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150301004331) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
