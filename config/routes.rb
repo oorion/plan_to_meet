9319d6fe0f7954da28e4add11f6b6bf80adb36b3
Orion Osborn <orion.osborn@gmail.com>
2015-03-02 16:22:32 -0700

    add oauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   resources :users do
     get 'preferences', to: 'users/preferences#index'
   end
 
-  post 'login', to: 'sessions#create', as: 'login'
-  delete 'logout', to: 'sessions#destroy', as: 'logout'
+#  post 'login', to: 'sessions#create', as: 'login'
+#  delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
