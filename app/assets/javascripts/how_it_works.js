483beaafcfb11dd57ce30d83e979fc24bcc656e3
Orion Osborn <orion.osborn@gmail.com>
2015-03-04 11:49:19 -0700

    update how-it-works section, add jquery for arrow, and update styling

--- app/assets/javascripts/how_it_works.js
+++ app/assets/javascripts/how_it_works.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+  function toggleAngles() {
+    if ($(".how-it-works i").hasClass("fa-angle-down")) {
+      $(".how-it-works i").removeClass("fa-angle-down");
+      $(".how-it-works i").addClass("fa-angle-up");
+    } else {
+      $(".how-it-works i").removeClass("fa-angle-up");
+      $(".how-it-works i").addClass("fa-angle-down");
+    }
+  };
+
   $(".how-it-works-content").hide();
+
   $(".how-it-works").on("click", function() {
-    $(".how-it-works-content").slideToggle("slow", function() {});
+    $(".how-it-works-content").slideToggle("slow", function() {
+      toggleAngles();
+    });
   });
 });
