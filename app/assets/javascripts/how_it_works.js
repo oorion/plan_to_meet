912648b7820eb9db8fa7b4762dbbf32a0c5f7c18
Orion Osborn <orion.osborn@gmail.com>
2015-03-03 22:13:04 -0700

    add styling to make how it works section slide underneath arrow

--- app/assets/javascripts/how_it_works.js
+++ app/assets/javascripts/how_it_works.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".how-it-works-content").hide();
-  $(".how-it-works-button").on("click", function() {
+  $(".how-it-works").on("click", function() {
     $(".how-it-works-content").slideToggle("slow", function() {});
   });
 });
