1150d0e3c1990434ba06438ceeb0013b53f91ba9
Orion Osborn <orion.osborn@gmail.com>
2015-03-07 19:15:23 -0700

    add twilio integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def text
+    twilio_sid = ENV["TWILIO_SID"]
+    twilio_auth_token = ENV["TWILIO_AUTH_TOKEN"]
+    @client = Twilio::REST::Client.new twilio_sid, twilio_auth_token
+    @client.account.messages.create({
+      from: "+15059338671",
+      to: "5059086351",
+      body: "blah"
+    })
+  end
 end
