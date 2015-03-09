3b85ecca6d8838536f276886c1886dfcfb5c7d96
Orion Osborn <orion.osborn@gmail.com>
2015-03-09 11:50:17 -0600

    add a way to count the number of times a word appears in all event descriptions
    I do not think this will work because it includes many words that should be considered stop words

--- lib/recommendation_engine.rb
+++ lib/recommendation_engine.rb
@@ -6,6 +6,22 @@ class RecommendationEngine
   end
 
   def recommend_events
+    event_descriptions = user.events.map {|event| event.description}
+
+    all_words = event_descriptions.map do |description|
+      description.split(" ")
+    end.flatten.map do |word|
+      word.downcase
+    end
+
+    sorted_words = all_words.group_by do |word|
+      word
+    end.map do |k, v|
+      [k, v.count]
+    end.sort_by do |element|
+      element[1]
+    end.reverse
+
     binding.pry
     ""
   end
