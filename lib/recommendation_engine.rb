26e2a77595f9c514f3d6107c0391c1e74db9de45
Orion Osborn <orion.osborn@gmail.com>
2015-03-10 12:58:20 -0600

    finish refactoring recommendation engine

--- lib/recommendation_engine.rb
+++ lib/recommendation_engine.rb
@@ -26,19 +26,8 @@ class RecommendationEngine
 
   def recommend_events
     #account for people that haven't rsvped to many events.  possibly base it off their interests and/or categories of their groups
-    top_event_keywords = top_x_terms_by_event(KEYWORDS_PER_EVENT)
-    top_sorted_terms_by_tfidf = []
-    top_event_keywords.each do |event_top_terms|
-      event_top_terms.each do |term|
-        if term[0].length > MINIMUM_KEYWORD_LENGTH
-          top_sorted_terms_by_tfidf << term
-        end
-      end
-    end
-    top_sorted_terms_by_tfidf = top_sorted_terms_by_tfidf.sort_by do |e|
-      -e[1]
-    end
-
+    top_keywords_by_event = top_x_terms_by_event(KEYWORDS_PER_EVENT)
+    top_keywords_by_tfidf = top_terms_by_tfidf
     binding.pry
   end
 
@@ -49,6 +38,20 @@ class RecommendationEngine
         tfidf_by_term[term] = model.tfidf(document, term)
       end
       tfidf_by_term.sort_by{|_,tfidf| -tfidf}[0..num_of_terms_per_event]
+    end
+  end
+
+  def top_terms_by_tfidf
+    top_sorted_terms_by_tfidf = []
+    top_event_keywords.each do |event_top_terms|
+      event_top_terms.each do |term|
+        if term[0].length > MINIMUM_KEYWORD_LENGTH
+          top_sorted_terms_by_tfidf << term
+        end
+      end
+    end
+    top_sorted_terms_by_tfidf = top_sorted_terms_by_tfidf.sort_by do |e|
+      -e[1]
     end
   end
 end
