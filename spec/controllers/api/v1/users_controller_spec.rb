f111fc770ce4ab31e15dd0717adac873a15a18d6
Orion Osborn <orion.osborn@gmail.com>
2015-03-18 15:16:42 -0600

    attempt to make test better

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, :type => :controller do
   describe "GET index" do
     it "can return the index" do
+      binding.pry
+      events = create_list(:event, 10)
       get :index, format: :json
 
       expect(response.status).to eq(200)
